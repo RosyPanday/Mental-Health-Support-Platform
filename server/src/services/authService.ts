06b96c8b06093613888ea23114c4b5ed5f54027c
RosyPanday <pandayrosy14@gmail.com>
2026-08-04 18:01:47 +0545

    fix: add email to user creation

--- server/src/services/authService.ts
+++ server/src/services/authService.ts
@@ -56,6 +56,7 @@ export class AuthService {
         {
           username: input.username,
           password: hashedPassword,
+          email:input.email,
           phoneNumber: input.phoneNumber,
           role: input.role,
         },
